e66d9c6782789b6ba77303575e743e60a2b99547
senaysenturk <senaysenturk@outlook.com>
2018-07-07 15:23:48 +0300

    search the movie on google

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(()=> {
     $('#araForm').on('submit', (e) => {
         let araFilm = $('#araFilm').val();
-        filmGetir(araFilm);
+        filmleriGetir(araFilm);
         e.preventDefault();
     });
 });
 
-function filmGetir(araFilm) {
+function filmleriGetir(araFilm) {
     axios.get('http://www.omdbapi.com?s=' + araFilm + '&apikey=321eaeda')
         .then((response) => {
             console.log(response);
@@ -14,20 +14,28 @@ function filmGetir(araFilm) {
             let output = '';
             $.each(movies, (index, movie) => {
                 output += `
-          <div class="col-md-3">
+          <div class="col-md-4">
             <div class="well text-center">
-              <img src="${movie.Poster}">
-              <h5>${movie.Title}</h5>
-              <h6>${movie.Year}</h6>
-              <a onclick="filmSecildi('${movie.imdbID}')" class="btn btn-primary" href="#">Film Detayı</a>
+               <p>
+                  <img src="${movie.Poster}">
+                  <h5 a onclick="filmSec('${movie.Title}')">${movie.Title}</h5>
+                  <h6 a onclick="filmSec('${movie.Title}')">${movie.Year}</h6>
+             </p>
+            
             </div>
           </div>
         `;
             });
 
-            $('#movies').html(output);
+            $('#filmler').html(output);
         })
         .catch((err) => {
             console.log(err.response);
         });
+}
+
+function filmSec(ad){
+    sessionStorage.setItem('filmAdi',ad);
+    window.open('http://google.com/search?tbm=vid&hl=tr&source=hp&biw=&bih=&q='+ad);
+    return false;
 }
